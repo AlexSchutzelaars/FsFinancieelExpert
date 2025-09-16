2568fa04e872e13b55be5fd6a4f51707a570030d
Alex Schutzelaars <a.schutzelaars@outlook.com>
2025-09-16 17:59:10 +0200

    Scherm voor inzicht in effectenportefeuille toegevoegd

---
+++ Effectenportefeuille.fs
@@ -0,0 +1,167 @@
+module EffectenPortefeuille
+open System.IO
+open System.Xml
+open System.Windows.Forms
+open System.Linq
+open System.Xml.Linq
+open System
+
+
+type Measurement = {MeasurementDate: string; HowMany: double; Price: double}
+type FundInfo = {Provider: string; Name: string; Description: string; Measurements: List<Measurement>}
+
+let getXmlFileName(providerInFileName: string) =
+    let fileName = $"C:/Projecten/FSharpWfVoorbeeld/FundInfo{providerInFileName}.xml"
+    fileName
+
+let getProvider(doc: XmlDocument)  =
+   let providerNodes = doc.SelectSingleNode("//provider/name") |> Seq.cast<XmlNode>
+   let provider = providerNodes.ToList().[0].Value.ToString()
+   provider
+
+let getFundNodes(doc: XmlDocument)  =
+   let fundNodes1 = doc.SelectNodes("//provider/funds/fund") |> Seq.cast<XmlNode>
+   let fundNodes2 = fundNodes1.ToList()
+   fundNodes2
+
+let getDataForFund(fundProviderInXml: string, fundNode: XmlNode) =
+   let results = XDocument.Parse(fundNode.OuterXml).Descendants().ToList()
+   let fundName = results.Where(fun n -> n.Name = XName.Get "Name").ToList().[0].Value
+   let description = results.Where(fun d -> d.Name = XName.Get "Description").ToList().[0].Value
+
+   let timeslices = results.Where(fun t -> t.Name = XName.Get "TimeSlice").ToList()
+   let mutable measurements: List<Measurement> = []
+
+   for timeslice in timeslices do
+      let timesliceResults = XElement.Parse(timeslice.ToString()).Descendants().ToList()
+      // let testje = timesliceResults.Where(fun node -> node.Name = XName.Get "Date" && node.Value = "2021-04-13").ToList()
+      let measurementDate = timesliceResults.Where(fun n -> n.Name = XName.Get "Date").ToList().[0].Value
+      let howMany = timesliceResults.Where(fun n -> n.Name = XName.Get "HowMany").ToList().[0].Value
+      let price = timesliceResults.Where(fun n -> n.Name = XName.Get "Price").ToList().[0].Value
+      let measurement: Measurement = {MeasurementDate = measurementDate; HowMany = double(howMany); Price = double(price)}
+      measurements <- measurement::measurements
+   
+   let mutable fundInfo = {Provider = fundProviderInXml; Name = fundName; Description = description;
+                            Measurements = measurements}
+   fundInfo
+
+let getDataForFundByName(providerInFileName: string, nameOfFund: string) =
+   let fileName = getXmlFileName(providerInFileName)
+
+   (* https://docs.microsoft.com/en-us/dotnet/api/system.xml.xmldocument?view=net-5.0 *)
+
+   let doc = new XmlDocument()
+   doc.Load fileName
+   let providerInXml = getProvider doc
+   let funds = getFundNodes doc
+   let mutable fundInfo = {Provider = providerInXml; Name = "Dummy"; Description = "Dummy"; Measurements = []}
+   let mutable found = false
+   for index = 0 to funds.Count - 1 do
+     if not found then
+         fundInfo <- getDataForFund(providerInXml, funds.[index])
+     if fundInfo.Name = nameOfFund then
+       found <- true
+   fundInfo
+
+let maakEffectenPortefeuilleFormulier () =
+    let frmEffectenportefeuille = new Form(Text = "Bekijk je effectenportefeuille", Width = 700, Height = 300)
+
+    let clientsize = new System.Drawing.Size(700, 430)
+    let fundsForm = new Form(MaximizeBox = false, Text = "Fonds-info", ClientSize = clientsize, StartPosition = FormStartPosition.CenterScreen)
+
+    // Maak een TextBox om het pad te tonen
+    let startY_ButtonXmlPath = 20
+
+    let btnXmlBestand = new Button(Text = "Kies XML-bestand voor effectenportefeuille", Left = 20, Top = startY_ButtonXmlPath, Width = 250)
+
+    let startY_TxtBoxXmlPath = startY_ButtonXmlPath + 40
+
+    let txtXmlBestand = new TextBox(ReadOnly = true, Height = 20, Top = startY_TxtBoxXmlPath, Left = 20, Width = 400)
+
+    let startY_DateTime = startY_TxtBoxXmlPath + 40
+    let dateTimePicker: DateTimePicker = new DateTimePicker(Left = 20, Top = startY_DateTime, Height = 20, Width = 200)
+
+    dateTimePicker.MinDate = new DateTime(2019, 1, 1) |> ignore
+
+    let startY_LBoxFunds = startY_DateTime + 40
+    let startY_ValueFunds = startY_LBoxFunds + 80
+    let lboxFunds = new ListBox(Left = 20, Top = startY_LBoxFunds, Height = 50, Width = 300)
+    let lblCalculatedForFund = new Label(Text = "Marktwaarde voor fonds", Left = 20, Top = startY_ValueFunds - 20, Height = 20, Width = 200)
+    let txtCalculatedForFund = new TextBox(Left = 20, Top = startY_ValueFunds, Height = 20, Width = 90, Visible = true)
+    let btnTerug = new Button(Text = "Terug naar financieel hoofdmenu", Left = 350, Top = startY_ValueFunds,Width = 200)
+
+        // Event handler voor de bestand-selectieknop
+    btnXmlBestand.Click.Add(fun _ ->
+        use dialog = new OpenFileDialog()
+        dialog.Filter <- "XML bestanden (*.xml)|*.xml|Alle bestanden (*.*)|*.*"
+        dialog.Title <- "Selecteer een XML-bestand"
+        if dialog.ShowDialog() = DialogResult.OK then
+            txtXmlBestand.Text <- dialog.FileName
+    )
+
+    btnXmlBestand.Click.Add(fun _ -> 
+        lboxFunds.Items.Clear()
+        txtCalculatedForFund.Visible <- false
+        let xmlFileName = txtXmlBestand.Text
+        if File.Exists xmlFileName then
+            let doc = new XmlDocument()
+            doc.Load xmlFileName
+            let provider = getProvider doc
+            let funds = getFundNodes doc
+            for index = 0 to funds.Count - 1 do
+                let fundInfo = getDataForFund(provider, funds.[index])
+                let fundName = fundInfo.Name
+                lboxFunds.Items.Add(fundName) |> ignore
+    )
+
+    let xmlFileName = txtXmlBestand.Text
+
+    if File.Exists xmlFileName then
+        let doc = new XmlDocument()
+        doc.Load xmlFileName
+        let provider = getProvider doc
+        let funds = getFundNodes doc
+        for index = 0 to funds.Count - 1 do
+            let fundInfo = getDataForFund(provider, funds.[index])
+            let fundName = fundInfo.Name
+            lboxFunds.Items.Add(fundName) |> ignore
+
+    let providerInFileName = "AsnBank"
+    lboxFunds.SelectedIndexChanged.Add(fun _ -> 
+                                        let theDate = dateTimePicker.Value.ToString("yyyy-MM-dd");
+                                        let nameOfFund = lboxFunds.SelectedItem.ToString()
+                                        let fundInfo = getDataForFundByName(providerInFileName, nameOfFund)
+
+                                        let fondsinfoOpDatum = fundInfo.Measurements.ToList().OrderByDescending(fun m -> m.MeasurementDate)
+                                        let mutable measurement = {MeasurementDate = "2019-01-01"; HowMany = 0.0; Price = 0.0}
+                                        for m in fondsinfoOpDatum do
+                                          if m.MeasurementDate <= theDate then
+                                            measurement <- m
+  
+                                        if measurement.MeasurementDate = "2019-01-01" then
+                                        // geen meting gevonden
+                                          txtCalculatedForFund.Visible <- false
+                                        else
+                                          let calculatedValue = (measurement.HowMany*measurement.Price)
+                                          txtCalculatedForFund.Visible <- true
+                                          txtCalculatedForFund.Text <- calculatedValue.ToString(".00")
+                                          let dialogHeader = $"Market value at {theDate}"
+                                          MessageBox.Show($"{fundInfo.Name} at {measurement.MeasurementDate}", dialogHeader) |> ignore)
+
+
+    btnTerug.Click.Add(fun _ -> frmEffectenportefeuille.Close())
+
+    fundsForm.Controls.Add dateTimePicker
+    fundsForm.Controls.Add btnXmlBestand
+    fundsForm.Controls.Add txtXmlBestand
+    fundsForm.Controls.Add lboxFunds
+    fundsForm.Controls.Add txtCalculatedForFund
+
+    frmEffectenportefeuille.Controls.Add dateTimePicker
+    frmEffectenportefeuille.Controls.Add btnXmlBestand
+    frmEffectenportefeuille.Controls.Add txtXmlBestand
+    frmEffectenportefeuille.Controls.Add lboxFunds
+    frmEffectenportefeuille.Controls.Add lblCalculatedForFund
+    frmEffectenportefeuille.Controls.Add txtCalculatedForFund
+    frmEffectenportefeuille.Controls.Add(btnTerug)
+    frmEffectenportefeuille
--- Program.fs
+++ Program.fs
@@ -2,11 +2,12 @@
 open System.Windows.Forms
 open HuidigeWaarde
 open ToekomstigeWaarde
-open NogTeBepalen
+open EffectenPortefeuille
 
 [<STAThread>]
+[<EntryPoint>]
 do
-    let form = new Form(Text = "FinancieelExpert2", Width = 400, Height = 300)
+    let form = new Form(Text = "FinancieelExpert v2", Width = 400, Height = 300)
 
     let schermPanel = new Panel(Top = 80, Left = 10, Width = 360, Height = 150)
     form.Controls.Add(schermPanel)
@@ -14,18 +15,18 @@ do
     let toonHoofdscherm () =
         schermPanel.Controls.Clear()
 
-        let knop1 = new Button(Text = "Toekomstige waarde", Top = 10, Left = 10, Width = 200)
-        knop1.Click.Add(fun _ -> (maakToekomstigeWaardeFormulier().ShowDialog() |> ignore))
+        let btnToekomstigeWaarde = new Button(Text = "Toekomstige waarde", Top = 10, Left = 10, Width = 200)
+        btnToekomstigeWaarde.Click.Add(fun _ -> (maakToekomstigeWaardeFormulier().ShowDialog() |> ignore))
 
-        let knop2 = new Button(Text = "Hudige waarde (HW)", Top = 50, Left = 10, Width = 200)
-        knop2.Click.Add(fun _ -> (maakHwFormulier().ShowDialog() |> ignore))
+        let btnEffectenPortefeuille = new Button(Text = "Effectenportefeuille", Top = 50, Left = 10, Width = 200)
+        btnEffectenPortefeuille.Click.Add(fun _ -> (maakEffectenPortefeuilleFormulier().ShowDialog() |> ignore))
 
-        let knop3 = new Button(Text = "Nog te bepalen", Top = 90, Left = 10, Width = 200)
-        knop3.Click.Add(fun _ -> (maaknogTeBepalenFormulier().ShowDialog() |> ignore))
+        let btnHuidigeWaarde = new Button(Text = "Huidige waarde (HW)", Top = 90, Left = 10, Width = 200)
+        btnHuidigeWaarde.Click.Add(fun _ -> (maakHwFormulier().ShowDialog() |> ignore))
 
-        schermPanel.Controls.Add(knop1)
-        schermPanel.Controls.Add(knop2)
-        schermPanel.Controls.Add(knop3)
+        schermPanel.Controls.Add(btnToekomstigeWaarde)
+        schermPanel.Controls.Add(btnEffectenPortefeuille)
+        schermPanel.Controls.Add(btnHuidigeWaarde)
 
     toonHoofdscherm()
     Application.Run(form)
